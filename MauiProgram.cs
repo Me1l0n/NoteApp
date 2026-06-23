09f54ee1ecdd84225e36340ffeed95a0bc185bc4
Me1l0n <gusk5236@gmail.com>
2026-06-23 10:33:00 +0300

    Initial commit: NoteApp with Camera, Geolocation and SQLite

---
+++ MauiProgram.cs
@@ -0,0 +1,23 @@
+﻿using NoteApp.Services;
+using NoteApp.Pages;
+
+namespace NoteApp;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>();
+
+        builder.Services.AddSingleton<DatabaseService>();
+        builder.Services.AddSingleton<LocationService>();
+
+        builder.Services.AddTransient<MainPage>();
+        builder.Services.AddTransient<AddNotePage>();
+        builder.Services.AddTransient<MapPage>();
+
+        return builder.Build();
+    }
+}

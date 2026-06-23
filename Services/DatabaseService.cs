59446dfb61e072984902ac6c2abd3fadc373b8c0
Me1l0n <gusk5236@gmail.com>
2026-06-23 10:33:07 +0300

    Initial commit: NoteApp with Camera, Geolocation and SQLite

---
+++ Services/DatabaseService.cs
@@ -0,0 +1,36 @@
+﻿using SQLite;
+using NoteApp.Models;
+
+namespace NoteApp.Services;
+
+public class DatabaseService
+{
+    private SQLiteConnection _db;
+
+    private void Init()
+    {
+        if (_db != null) return;
+
+        string dbPath = Path.Combine(FileSystem.AppDataDirectory, "notes.db3");
+        _db = new SQLiteConnection(dbPath);
+        _db.CreateTable<Note>();
+    }
+
+    public List<Note> GetNotes()
+    {
+        Init();
+        return _db.Table<Note>().ToList();
+    }
+
+    public void AddNote(Note note)
+    {
+        Init();
+        _db.Insert(note);
+    }
+
+    public void DeleteNote(Note note)
+    {
+        Init();
+        _db.Delete(note);
+    }
+}

0b4469f4c12138213a36cc96b582d06243cfcd11
Me1l0n <gusk5236@gmail.com>
2026-06-23 10:33:31 +0300

    Initial commit: NoteApp with Camera, Geolocation and SQLite

---
+++ AppShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using NoteApp.Pages;
+
+namespace NoteApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+
+        Routing.RegisterRoute(nameof(AddNotePage), typeof(AddNotePage));
+        Routing.RegisterRoute(nameof(MapPage), typeof(MapPage));
+    }
+}

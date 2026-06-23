0aa083fa12f5fa13387a79ebc09e8389e2fa8358
Me1l0n <gusk5236@gmail.com>
2026-06-23 10:33:09 +0300

    Initial commit: NoteApp with Camera, Geolocation and SQLite

---
+++ Services/LocationService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Maui.Devices.Sensors;
+
+namespace NoteApp.Services;
+
+public class LocationService
+{
+    public async Task<Location> GetCurrentLocationAsync()
+    {
+        try
+        {
+            var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+            var location = await Geolocation.Default.GetLocationAsync(request);
+
+            if (location == null)
+            {
+                return await Geolocation.Default.GetLastKnownLocationAsync();
+            }
+
+            return location;
+        }
+        catch (FeatureNotSupportedException)
+        {
+            return null;
+        }
+        catch (PermissionException)
+        {
+            return null;
+        }
+        catch
+        {
+            return await Geolocation.Default.GetLastKnownLocationAsync();
+        }
+    }
+}

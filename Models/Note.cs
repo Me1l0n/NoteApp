90f4100de3b70344849d2df88adccdfefeb822dc
Me1l0n <gusk5236@gmail.com>
2026-06-23 10:33:05 +0300

    Initial commit: NoteApp with Camera, Geolocation and SQLite

---
+++ Models/Note.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace NoteApp.Models;
+
+public class Note
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string PhotoPath { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
